ae737391582642a167de203103e997e1b347b75e
TheRedSea07 <tongmoses727@gmail.com>
2023-10-24 16:46:30 +0100

    Successfully located the required file

--- src/BareBonesInterpreter.java
+++ src/BareBonesInterpreter.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileReader;
+
 public class BareBonesInterpreter {
+
+    public static void readFromFile() {
+
+        //TODO:
+        //Read file char by char (incl. EOF char)
+        try {
+            File fileName = new File("./src/test.txt");
+            FileReader fileReader = new FileReader(fileName);
+            System.out.println("Hello, World!");
+            fileReader.close();
+        }
+        catch (Exception e) {
+            System.out.println("File is not found");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World");
+        readFromFile();
     }
 }
